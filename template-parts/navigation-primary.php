e59da5f5ce943c9dec1a484be55934c574b4b23a
benBen <benmbenjamin@gmail.com>
2019-06-15 13:40:25 -0400

    lil nav styles

--- template-parts/navigation-primary.php
+++ template-parts/navigation-primary.php
@@ -25,7 +25,12 @@ $theme = get_field( 'theme' );
       aria-controls="primary-menu"
       :aria-expanded="open"
       v-on="{ click : open ? toggleClosed : toggleOpen }">
-      <?php esc_html_e( 'Primary Menu', 'paperplanes' ); ?>
+      <svg width="20" height="20" viewBox="0 0 100 100">
+        <path d="M0 27.5 L100 27.5" stroke-width="8" transform-origin="100 27.5"
+              v-bind:style="{ stroke: 'rgba('+255*theme+','+255*theme+','+255*theme+',1)' }"/>
+        <path d="M0 72.5 L100 72.5" stroke-width="8" transform-origin="100 72.5"
+              v-bind:style="{ stroke: 'rgba('+255*theme+','+255*theme+','+255*theme+',1)' }"/>
+      </svg>
     </button>
   </div>
   <?php
