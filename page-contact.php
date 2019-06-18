4d5d8b98044fdaec8e8b94392bcbb4d9a001cd3d
benBen <benmbenjamin@gmail.com>
2019-06-18 15:40:08 -0400

    wip contact

--- page-contact.php
+++ page-contact.php
@@ -10,35 +10,37 @@ $content = get_field( 'content' );
 if ( have_rows( 'content' ) ) :
   while ( have_rows( 'content' ) ) : the_row();
     if ( get_row_layout() == 'banner' ) : ?>
-      <h1><?php echo get_sub_field( 'text' ) ?></h1> <?php
+      <div class="layout banner">
+        <div class="text"><?php echo get_sub_field( 'text' ) ?></div>
+      </div> <?php
     endif;
     if ( get_row_layout() == 'grid' ) : ?>
-    <div class="grid">
+    <div class="layout grid">
       <?php
       if ( have_rows( 'items' ) ) :
         while ( have_rows( 'items' ) ) : the_row();
           $layout = get_row_layout();
           if ( $layout == 'contact' ) : ?>
             <span class="item <?php echo $layout ?>">
-              <h5><?php echo get_sub_field( 'label' ) ?></h5>
-              <a href="<?php echo get_sub_field( 'link' ) ?>">
+              <h5 class="label"><?php echo get_sub_field( 'label' ) ?></h5>
+              <a class="link" href="<?php echo get_sub_field( 'link' ) ?>">
                 <?php echo get_sub_field( 'display' ) ?>
               </a>
             </span>
           <?php endif;
           if ( $layout == 'tag' ) : ?>
             <span class="item <?php echo $layout ?>">
-              <h5><?php echo get_sub_field( 'text' ) ?></h5>
+              <h5 class="text"><?php echo get_sub_field( 'text' ) ?></h5>
             </span>
           <?php endif;
           if ( $layout == 'social' ) : ?>
             <span class="item <?php echo $layout ?>">
-              <h5><?php echo get_sub_field( 'label' ) ?></h5>
+              <h5 class="label"><?php echo get_sub_field( 'label' ) ?></h5>
               <?php
                 if ( have_rows( 'links' ) ) :
                   while ( have_rows( 'links' ) ) : the_row(); ?>
-                  <a href="<?php echo get_sub_field( 'link' ) ?>">
-                    <img src="<?php echo get_sub_field( 'icon' )['url'] ?>" />
+                  <a class="link" href="<?php echo get_sub_field( 'link' ) ?>">
+                    <img class="icon" src="<?php echo get_sub_field( 'icon' )['url'] ?>" />
                   </a><?php
                   endwhile;
                 endif;
